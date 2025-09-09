85439b126d7e53e75941f581d7d94edd33114cdc
linxu <devinlin9679@gmail.com>
2025-09-09 19:57:21 +0800

    fix: fix Github NextAuth

--- auth.ts
+++ auth.ts
@@ -2,8 +2,8 @@
  * @Description: 认证配置
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2025-09-08 16:02:00
- * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2025-09-09 18:56:15
+ * @LastEditors: linxu devinlin9679@gmail.com
+ * @LastEditTime: 2025-09-09 19:56:49
  */
 // import NextAuth from "next-auth"
  
@@ -11,8 +11,8 @@
 //   providers: [],
 // })
 
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
+// import NextAuth from "next-auth"
+// import GitHub from "next-auth/providers/github"
 
 // 解决nodejs无法使用代理的问题
 // import { HttpsProxyAgent } from "https-proxy-agent";
@@ -20,19 +20,19 @@ import GitHub from "next-auth/providers/github"
 // const proxyAgent = new HttpsProxyAgent("http://127.0.0.1:7899");
 // process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [
-    GitHub({
-      clientId: process.env.AUTH_GITHUB_ID!,
-      clientSecret: process.env.AUTH_GITHUB_SECRET!,
-    }),
-  ],
-  secret: process.env.AUTH_SECRET,
-})
-
-// import NextAuth from "next-auth"
-// import GitHub from "next-auth/providers/github"
- 
 // export const { handlers, auth, signIn, signOut } = NextAuth({
-//   providers: [GitHub],
+//   providers: [
+//     GitHub({
+//       clientId: process.env.AUTH_GITHUB_ID!,
+//       clientSecret: process.env.AUTH_GITHUB_SECRET!,
+//     }),
+//   ],
+//   secret: process.env.AUTH_SECRET,
 // })
+
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+ 
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [GitHub],
+})
